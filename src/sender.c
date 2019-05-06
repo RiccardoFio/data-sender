4cce85fc229f37f1e336081af7c04b54468bf4e3
Riccardo <riccardofio97@gmail.com>
2019-05-06 12:13:29 +0200

    Aggiunti file

---
+++ src/sender.c
@@ -0,0 +1,20 @@
+#include "sender.h"
+
+static int _sender_data_is_valid(char *sender, char *destination, char *message);
+static int _sender_send_message(char *sender, char *destination, char *message);
+
+
+int sender_send_data(char *sender, char *destination, char *message)
+{
+    
+}
+
+static int _sender_data_is_valid(char *sender, char *destination, char *message)
+{
+
+}
+
+static int _sender_send_message(char *sender, char *destination, char *message)
+{
+
+}
---
+++ src/sender.h
@@ -0,0 +1,6 @@
+#ifndef SENDER_H
+#define SENDER_H
+
+int sender_(char *sender, char *destination, char *message);
+
+#endif
